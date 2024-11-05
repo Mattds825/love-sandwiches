1adf3debef1a355582e23c63bf543e77fc7bf8dc
Matthew Da Silva <matthewgdasilva25@gmail.com>
2024-11-05 14:59:01 +0000

    add function to get sales dat from user input and a function to validate the user input

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-import gspread 
+import gspread
 from google.oauth2.service_account import Credentials
 
 # Defining the scope
@@ -18,10 +18,39 @@ GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 
 # Accessing the Google Sheet
 SHEET = GSPREAD_CLIENT.open('love_sandwiches')
-    
-# Accessing the sales worksheet
-sales = SHEET.worksheet('sales')
 
-# Accessing the data
-data = sales.get_all_values()
-print(data)
+
+def get_sales_data():
+    """
+    Get sales figures input from the user    
+    """
+
+    print("Please enter sales data from the last market.")
+    print("Data should be six numbers, separated by commas.")
+    print("Example: 10,20,30,40,50,60 \n")
+
+    data_str = input("Enter your data here: ")
+
+    sales_data = data_str.split(",")
+    validate_data(sales_data)
+
+
+def validate_data(values):
+    """
+    Inside the try convert all string values into integers.
+    Raise ValueError if strings cannot be converted into int,
+    or if there are not exactly 6 values.
+    """
+    
+    try:
+        [int(value) for value in values]
+        if len(values) != 6:
+            raise ValueError(
+                f"Exactly 6 values required, you provided {len(values)}"
+            )
+    except ValueError as e:
+        print(f"Invalid data: {e}, please try again. \n")
+        
+
+
+get_sales_data()
